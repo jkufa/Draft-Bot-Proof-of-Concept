0402aced0a60a41ed091e62a66fddf679c9b5b71
Jack Kufa <jack@kufa.io>
2020-11-01 19:50:56 -0600

    Implement boilerplate bot code

---
+++ main.py
@@ -0,0 +1,57 @@
+import discord
+import json
+
+# load in JSON files
+with open('bot-token.json','r') as myfile:
+  data = myfile.read()
+token_data = json.loads(data)
+with open('config.json','r') as myfile:
+  data = myfile.read()
+config_data = json.loads(data)
+
+client = discord.Client()
+token = token_data['token']
+prefix  = config_data['prefix']
+
+@client.event
+async def on_ready():
+  print('Logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  # Return if message is sent by bot
+  if(message.author == client.user):
+    return
+  # See if command is being run
+  if(message.content.startswith(prefix)):
+    args = message.content[1:].split(' ')
+    if(args[0] == ''):
+      await message.channel.send("ERROR: No command was given!")
+      return
+    command = args.pop(0) # Pop first index, it will always be the command
+
+    #See what command was given
+    if(command.lower() == "select"):
+      await message.channel.send("TODO: !" + command.lower())
+      await message.channel.send(args[0])
+    elif(command.lower() == "submit"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "redraft"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "delete"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "userinfo"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "rankings"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "matchesplayed"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "pokemon"):
+      await message.channel.send("TODO: !" + command.lower())
+    elif(command.lower() == "average" or command.lower() == "avg"):
+      await message.channel.send("TODO: !" + command.lower())
+    else:
+      await message.channel.send("Hey, that's not a valid command!")
+
+
+client.run(token)
