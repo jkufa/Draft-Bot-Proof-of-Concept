c79e4a4e96d7a68af27ba63716a68cb9b59dee3c
Jack <jhkmw3@mst.edu>
2020-11-30 12:44:49 -0600

    Fix shell script paths & add to manage league page

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,11 +1,13 @@
-import os
+import os, re
 from flask import Flask, render_template, request, redirect
-from web_app.py.queries import Insert
+from web_app.py.queries import Query
 
 file_path = os.path.abspath(os.getcwd())+"/db/pokemon_draft_league.db"
 app = Flask(__name__)
 app.debug=True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ file_path
+q = Query(file_path)
+
 
 @app.route('/')
 def index():
@@ -26,7 +28,6 @@ def contact():
 @app.route('/create_league', methods=["GET", "POST"])
 def create_league():
   if request.method == "POST":
-    ins = Insert(file_path)
     req = request.form
     # POST variables
     league_name = req.get("leaguename")
@@ -50,14 +51,19 @@ def create_league():
         is_admin.append(False)
     # Send data to database
     print(users, timezones, is_coach, is_admin)
-    ins.ins_league(league_name, league_format, tierlist)
-    ins.ins_users(users,timezones,is_coach,is_admin,league_name)
+    q.ins_league(league_name, league_format, tierlist)
+    q.ins_users(users,timezones,is_coach,is_admin,league_name)
     return redirect(request.url)
   return render_template('./admin/create_league.html')
 
 @app.route('/manage_leagues', methods=["GET", "POST"])
 def manage_leagues():
-  return render_template('./admin/manage_leagues.html')
+  lgs = q.select_leagues()
+  lg_names = []
+  for lg in lgs:
+    lg_names.append(re.sub("[(),']",'',str(lg.name)))
+  print(lg_names)
+  return render_template('./admin/manage_leagues.html', lg_names=lg_names)
 
 if __name__ == '__main__':
   app.run()
